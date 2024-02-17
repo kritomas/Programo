3a19ab3d8981a048c6aa6100ed94402fd01c455e
kritomas <Unknown>
2024-02-17 14:15:50 +0100

    Database setup.

---
+++ setup.sql
@@ -0,0 +1,40 @@
+use krizko;
+
+create table Lang
+(
+	id int primary key identity(1, 1),
+	name varchar(40) not null
+);
+
+create table Programmer
+(
+	id int primary key identity(1, 1),
+	username varchar(100) not null,
+);
+
+create table Certification
+(
+	id int primary key identity(1, 1),
+	lang_id int foreign key references Lang(id) not null,
+	programmer_id int foreign key references Programmer(id) not null,
+	date_start datetime not null,
+	date_end datetime not null,
+	check(date_end >= date_start)
+);
+
+create table Project
+(
+	id int primary key identity(1, 1),
+	name varchar(250) not null,
+	is_abandoned bit not null
+);
+
+create table Work
+(
+	id int primary key identity(1, 1),
+	project_id int foreign key references Project(id) not null,
+	programmer_id int foreign key references Programmer(id) not null,
+	date_start datetime not null,
+	date_end datetime not null,
+	check(date_end >= date_start)
+);
